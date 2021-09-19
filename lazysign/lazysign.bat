38bf6cee2868eb36e26a4c601b68ffbb05975435
Mofisto <>
2021-09-19 21:05:52 +0800

    Add timestamp algorithm and modify the error of having spaces in the organization

--- lazysign/lazysign.bat
+++ lazysign/lazysign.bat
@@ -3,20 +3,20 @@ ECHO ==========================
 ECHO Creating cert ...
 set /p Company="Please provide the company name you want to sign as: "
 ECHO ===========================
-.\makecert.exe -len 2048 %Company%.cer -n "CN="%Company%  -r -sv %Company%.pvk
+.\makecert.exe -len 2048  "%Company%.cer" -n "CN=%Company%"  -r -sv "%Company%.pvk"
 ECHO ==========================
 ECHO Creating pfx from cert ...
 ECHO ===========================
-.\pvk2pfx.exe -pvk %Company%.pvk -spc %Company%.cer -pfx %Company%.pfx
+.\pvk2pfx.exe -pvk "%Company%.pvk" -spc "%Company%.cer" -pfx "%Company%.pfx"
 ECHO ==========================
 ECHO Signing time....
 ECHO ===========================
 set /p BinToSign="Please provide the path to the binary to sign: "
-.\signtool.exe sign /f .\%Company%.pfx /t http://timestamp.comodoca.com/authenticode %BinToSign%
+.\signtool.exe sign /fd sha256 /f ".\%Company%.pfx" /t http://timestamp.comodoca.com/authenticode %BinToSign%
 ECHO ==========================
 ECHO Cleaning up...
 ECHO ===========================
-del %Company%.pvk
-del %Company%.cer
-del %Company%.pfx
+del "%Company%.pvk"
+del "%Company%.cer"
+del "%Company%.pfx"
 PAUSE
